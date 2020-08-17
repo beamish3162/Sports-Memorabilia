ea63fc01efe293109546704bf1fd92153f4e45a2
David  Beamish <davidrbeamish@gmail.com>
2020-08-17 15:32:57 +0000

    trying deployment again

--- american_sports/settings.py
+++ american_sports/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '*yg-op3z+j_$+bo+_o3y4tfq79h+#^(j)jrc7$#oz7d8659od='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
